29cf38349e5d97e60322833363e01b105db0edf6
KrisVerses <krisincode@email.com>
2025-03-22 23:05:49 -0500

    fix: added search wrapper to fix glitching after every key stroke

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,19 +1,13 @@
-import PostCard from "@/components/ui/PostCard";
-import { allPosts } from "contentlayer/generated";
-import Link from "next/link";
+import SearchWrapper from "@/components/SearchWrapper";
 
 function PostIndex() {
-  const posts = allPosts.sort((a, b) => b.date.localeCompare(a.date));
-
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
       <h1 className="text-2xl font-bold tracking-tight text-gray-900">
         All Posts
       </h1>
-      <ul className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {posts.map((post) => (
-          <PostCard key={post._id} post={post} />
-        ))}
+      <ul className="mt-8 grid grid-cols-2 gap-8">
+        <SearchWrapper />
       </ul>
     </div>
   );
