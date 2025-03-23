a2a05c8b55adb65862db8637f0cf5254b2705b3b
KrisVerses <krisincode@email.com>
2025-03-23 01:40:04 -0500

    fix: corrected import

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,14 +1,16 @@
-import SearchWrapper from "@/components/SearchWrapper";
+import SearchWrapper from "@/components/ui/SearchWrapper";
 
 function PostIndex() {
   return (
-    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h1 className="text-2xl font-bold tracking-tight text-gray-900">
-        All Posts
-      </h1>
-      <ul className="mt-8 grid grid-cols-2 gap-8">
-        <SearchWrapper />
-      </ul>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-5xl lg:px-8">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+          All Posts
+        </h1>
+        <ul className="mt-8 grid grid-cols-1 gap-8">
+          <SearchWrapper />
+        </ul>
+      </div>
     </div>
   );
 }
