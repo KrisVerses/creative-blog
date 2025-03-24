fe88d7d11be6ca7519e782360177a99571946b1e
KrisVerses <krisincode@email.com>
2025-03-24 09:25:01 -0500

    refactor: moved layout imported components to layout.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import Footer from "@/components/ui/Footer";
 import PostIndex from "./post/page";
-import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <div className="max-w-5xl mx-auto">
-      <header>
-        <Navbar />
-      </header>
       <main>
         <PostIndex />
       </main>
-      <footer>
-        <Footer />
-      </footer>
     </div>
   );
 }
